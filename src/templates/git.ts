61294857d8a13cea99229e82c42b0097866c34ee
Brage Allum <brageallum@gmail.com>
2021-04-17 19:21:58 +0200

    Add /.idea to .gitignore for git template

--- src/templates/git.ts
+++ src/templates/git.ts
@@ -1,5 +1,12 @@
-import { exec, log } from '@lib';
+import { exec, log, write } from '@lib';
 
 log('Setting up git..');
 
 await exec('git init');
+
+write(
+    '.gitignore',
+    `# IDE files
+/.idea
+`,
+);
